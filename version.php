ae52b882d7324ef7a1d6acec97d6b57781e4e71c
Leo Auri <code@leoauri.com>
2020-07-06 03:29:38 +0000

    Fix Chrome visual plan columns collapsing bug
    Chrome doesn't fix table column widths set in colgroups, unlike
    Firefox.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_room';
-$plugin->release = '3.4-beta';
-$plugin->version  = 2020062900;
+$plugin->release = '3.4.1-beta';
+$plugin->version  = 2020070600;
 $plugin->requires = 2019111800;
 $plugin->maturity = MATURITY_ALPHA;
